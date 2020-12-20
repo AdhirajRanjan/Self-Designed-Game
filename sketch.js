57b806593eca3ba43519bd0e7076121a30b9c952
AdhirajRanjan <71758327+AdhirajRanjan@users.noreply.github.com>
2020-12-20 12:21:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 
 var player, playerimg, vehicle, vehicleimg;
+var obstacleimg;
+var score;
 var gameState = "intro"
 var edges;
 function preload()
@@ -29,9 +31,11 @@ function draw()
     player.collide(edges[2])
     player.collide(edges[3])
     
+
     edges.visible = false;
     if(gameState === "intro")
     {
+      stroke("blue")
       text("Use The arrow keys to move",230,100)
       text("Enter the plane",230,120)
       if(player.collide(edges[0])||player.collide(edges[1])||player.collide(edges[2])||player.collide(edges[3]))
@@ -41,6 +45,7 @@ function draw()
       }
       if(player.isTouching(vehicle))
       {
+        stroke("blue")
         text("Press space to enter the plane",400,270)
         if(keyDown("space"))
         {
@@ -74,10 +79,19 @@ function draw()
 
   if(gameState === "playing")
   {
-    text("Avoid the Obstacles",650,100)
-    vehicle.velocityX = 6;
-    camera.x = vehicle.x
+    score = Math.round(frameCount)
+    stroke("blue")
+    text("Avoid the Obstacles",200,100)
+    stroke("green")
+    text("Score: "+score,20,50)
+    //vehicle.velocityX = 6;
+    //camera.x = vehicle.x
     player.visible = false
+    vehicle.collide(edges[0])
+    vehicle.collide(edges[1])
+    vehicle.collide(edges[2])
+    vehicle.collide(edges[3])
+    
     if(keyIsDown(UP_ARROW))
     {
       vehicle.y = vehicle.y - 8
@@ -93,7 +107,36 @@ function draw()
         vehicle.x = vehicle.x + 4
 
     }
+    if(keyIsDown(LEFT_ARROW))
+    {
+        vehicle.x = vehicle.x - 4
+    }
+    if(score === 5000)
+    {
+      //change the image to car
+      gameState = "car"
+    }
   }
-    
+
+    if(gameState === "car")
+    {
+      stroke("blue")
+      text("You have now transformed into a car, you can only jump now")
+      score = Math.round(frameCount)
+    }
+    if(gameState === "playing")
+    {
+      spawnObstacles()
+    }
     drawSprites()
+}
+
+function spawnObstacles()
+{
+  if(frameCount % 30 === 0)
+  {
+    var obstacle = createSprite(640,Math.round(random(50,550)),30,30)
+    obstacle.velocityX = -(6 + 3 * score/500)
+  }
+
 }
